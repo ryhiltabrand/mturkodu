d6813f06599a69cbbbfdfdf0fedfd25c61f03514
ryhiltabrand <ryhiltabrand99@gmail.com>
2021-09-01 13:31:30 -0400

    auth

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 function App() {
   return (
     <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
-      </header>
-      <AmplifySignOut />
+      Hello
     </div>
   );
 }
